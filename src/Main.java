43b6718c18870019a5e10931d7cf71f0163798d8
mohammed <mohammedre4a@gmail.com>
2022-11-14 10:44:43 +0200

    feat: create simple main to demonstrate composable.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,22 @@
+import composite.Box;
+import composite.Composable;
+import composite.Product;
 
 public class Main {
     public static void main(String[] args) {
+        //composite pattern is used when we can represent our system's state as tree
+        //create the biggest object
+        Box biggestBox = new Box();
+        //add some products
+        biggestBox.addComposable(new Product(130, "Product A"));
+        biggestBox.addComposable(new Product(10, "Product B"));
+        biggestBox.addComposable(new Product(45, "Product C"));
+        //add another composite box
+        Box anotherBox = new Box();
+        anotherBox.addComposable(new Product(30, "Product D"));
+        anotherBox.addComposable(new Product(6, "Product E"));
+        biggestBox.addComposable(anotherBox);
 
+        biggestBox.getPrice();
     }
 }
