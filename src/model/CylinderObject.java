1ee698918bc72ea181c3a63fae2aebb1c610f6fc
mohammed <mohammedre4a@gmail.com>
2022-11-15 16:15:40 +0200

    feat: define a client-compatible class

---
+++ src/model/CylinderObject.java
@@ -0,0 +1,17 @@
+package model;
+
+//Cylinder is compatible with the Round hole.
+public class CylinderObject {
+    public CylinderObject() {
+    }
+
+    int radius;
+
+    public int getRadius() {
+        return radius;
+    }
+
+    public CylinderObject(int radius) {
+        this.radius = radius;
+    }
+}
