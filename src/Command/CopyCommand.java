5e8128f6c1811ddaf378e0da51e106f83b620af7
mohammed <mohammedre4a@gmail.com>
2022-11-15 23:03:12 +0200

    feat: create Command interface and 2 concrete impl.

---
+++ src/Command/CopyCommand.java
@@ -0,0 +1,8 @@
+package Command;
+
+public class CopyCommand implements Command{
+    @Override
+    public void execute() {
+
+    }
+}
---
+++ src/Command/SaveCommand.java
@@ -0,0 +1,8 @@
+package Command;
+
+public class SaveCommand implements Command{
+    @Override
+    public void execute() {
+
+    }
+}
